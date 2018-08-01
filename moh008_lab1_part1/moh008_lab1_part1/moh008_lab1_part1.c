1682f7db6df51c1e24779c8902e942327dafd3fd
moh008 <moh008@ucr.edu>
2018-07-31 19:33:19 -0700

    up

--- moh008_lab1_part1/moh008_lab1_part1/moh008_lab1_part1.c
+++ moh008_lab1_part1/moh008_lab1_part1/moh008_lab1_part1.c
@@ -9,34 +9,30 @@
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
 	
 	unsigned char tmpA = 0x00;
-	
+	unsigned char tmpB = 0x00;
 	while(1)
 	{
 		tmpA = PINA & 0x0F;
-		if(tmpA == 0x0F)
+		if(tmpA == 0x00)
 		{
-			cntavail = (cntavail & 0x00) | 0x80;
+			tmpB = 0x00;
 		}
-		else if(tmpA == 0x01 || tmpA == 0x02 || tmpA == 0x04 || tmpA == 0x08)
+		else if(tmpA == 0x01)
 		{
-			cntavail = (cntavail & 0x00) | 0x03;
+			tmpB = 0x01;
 		}
-		else if(tmpA == 0x0C || tmpA == 0x0A || tmpA == 0x09 || tmpA == 0x06 || tmpA == 0x05 || tmpA == 0x03)
+		else if(tmpA == 0x02)
 		{
-			cntavail = (cntavail & 0x00) | 0x02;
+			tmpB = 0x00;
 		}
-		else if(tmpA == 0x0E || tmpA == 0x0D || tmpA == 0x0B || tmpA == 0x07)
+		else if(tmpA == 0x03)
 		{
-			cntavail = (cntavail & 0x00) | 0x01;
+			tmpB = 0x00;
 		}
-		else if(tmpA == 0x00)
-		{
-			cntavail = (cntavail & 0x00) | 0x04;
-		}
-		PORTC = cntavail;
+		PORTB = tmpB;
 	}
 	return 0;
 }
